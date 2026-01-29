c25c255e8e756eadf894b642b0048f3765eb3c56
Daniel Schäftner <daniel.schaeftner@schaeftner-engineering.com>
2026-01-29 18:59:20 +0100

    first lesson

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,44 @@
-print("Hello, world! for git")
-print("This is an additional line for testing.")
+""" A simple hello world program.)"""
+''' A multi-line comment
+    that describes the purpose'''
+
+########### Hello World Program ############
+### Author: Your Name Here ###
+### Date: Today's Date Here ### 
+############################################
+
+# print("Hello, world! for git") #single line comment
+# print("This is an additional line for testing.")
+
+
+# variable = "test"
+
+# VariAble_11212 = "test2"
+
+# snake_case = "this_is_snake_case"
+# studenten_name_dhbw = "Max Mustermann"
+# camelCase = "thisIsCamelCase"
+# studentenNameDhbw = "Erika Mustermann"
+# PascalCase = "ThisIsPascalCase"
+# StudentenNameDhbw = "Hans Müller"
+
+var = "Daniel" 
+var2 = "Schäftner"
+
+vorname = "Daniel"
+nachname = "Schäftner"
+
+var1 = 1 # 0b0000 0001
+var2 = 2.1
+var3 = "Dany"
+
+var1 = "gleich  !  "
+var2 = "feierabend"
+
+var = "test " + var1 + var2 + 5
+
+print(var)
+
+
+
+
