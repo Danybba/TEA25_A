778fe8d79273ac34b6f0ea85d68523d5b91ab8a5
Alexander Hoppe <alexander.hoppe10@web.de>
2026-01-29 16:13:44 +0100

    Vokabeltrainer fertig

--- Karteikarten.py
+++ Karteikarten.py
@@ -1,4 +1,13 @@
-frage = []
-antwort = []
-lf = frage.length
-la = antwort.length
+frage = open("Karteikartentest_Fragen.csv", "r").read().splitlines()
+antwort = open("Karteikartentest_Antworten.csv", "r").read().splitlines()
+lf = len(frage)
+la = len(antwort)
+
+if lf == la:
+    for i in range(lf):
+        print("Frage:", frage[i])
+        user_input = input("Deine Antwort: ")
+        if user_input.lower() == antwort[i].lower():
+            print("Richtig!")
+        else:
+            print("Falsch! Die richtige Antwort ist:", antwort[i])
